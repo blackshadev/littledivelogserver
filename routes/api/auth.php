073ec07651bd35be252f05c308eee1086058bd68
Vincent Hagen <vinnie@script4web.nl>
2020-10-17 13:03:23 +0200

    CS

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\Api\AuthController;
 
 Route::get('/sessions', [AuthController::class, 'listSessions']);
 Route::post('/sessions', [AuthController::class, 'login']);

5948c8e1724bfd770589c4a614592ef42d22f98a
Vincent Hagen <vinnie@script4web.nl>
2023-01-22 16:16:54 +0100

    Implement pacefinder in typesense

--- app/Services/Places/TypesensePlaceFinder.php
+++ app/Services/Places/TypesensePlaceFinder.php
@@ -6,33 +6,34 @@ namespace App\Services\Places;
 
 use App\Application\Places\CommandObjects\FindPlaceCommand;
 use App\Application\Places\Services\PlaceFinder;
+use App\Domain\Places\Entities\Place;
+use App\Domain\Support\Arrg;
 use App\Models\Place as PlaceModel;
-use Typesense\Client;
+use Typesense\LaravelTypesense\Typesense;
 
 final class TypesensePlaceFinder implements PlaceFinder
 {
-    public function __construct(private readonly Client $client)
+    public function __construct(private readonly Typesense $typesense)
     {
     }
 
     public function find(FindPlaceCommand $command): array
     {
-        $q = [
-            'searches' => [
-                [
-                    'q' => $command->getKeywords(),
-                    'filter_by' => "created_by:" . $command->getUserId(),
-                ],
-                [
-                    'q' => $command->getKeywords(),
-                ],
-            ],
-        ];
-        $result = $this->client->getMultiSearch()->perform($q, [
-            'collection' => (new PlaceModel())->searchableAs(),
-            'query_by' => implode(',', (new PlaceModel())->typesenseQueryBy()),
+        $model = new PlaceModel();
+        $results = $this->typesense->getCollectionIndex($model)->getDocuments()->search([
+            'q' => $command->getKeywords(),
+            'sort_by' => sprintf('_eval(created_by:%d):desc,_text_match:desc', $command->getUserId()),
+            'query_by' => implode(',', $model->typesenseQueryBy()),
         ]);
 
-        dd($result);
+        return Arrg::map(
+            $results['hits'],
+            static fn ($result) => Place::existing(
+                id: (int)$result['document']['id'],
+                createdBy: (int)$result['document']['created_by'],
+                name: $result['document']['name'],
+                countryCode: $result['document']['country_code']
+            )
+        );
     }
 }

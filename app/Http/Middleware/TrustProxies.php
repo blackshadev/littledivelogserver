e3793ea9f97bfb38aa0a0c5f2a20949cb75405d4
Vincent Hagen <vinnie@script4web.nl>
2023-08-03 10:51:04 +0200

    Update docker container to php8.2 and bumped all composer

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -4,28 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
-use Illuminate\Contracts\Config\Repository;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 final class TrustProxies extends Middleware
 {
-    protected $proxies;
-
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
-
-    public function __construct(Repository $config)
-    {
-        parent::__construct($config);
-
-        $trustedProxies = env('TRUSTED_PROXIES');
-        if (!empty($trustedProxies)) {
-            $this->proxies = explode(',', $trustedProxies);
-        }
-    }
 }
